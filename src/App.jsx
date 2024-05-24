9ad2e75dcab04876acf9084dafae026c496e2cf1
Archit Kakkar <education.archit@gmail.com>
2024-05-24 17:46:59 +0530

    Small UI changes.

--- src/App.jsx
+++ src/App.jsx
@@ -25,28 +25,41 @@ function App() {
     passwordGenerator();
   }, [length, numberAllowed, characterAllowed, passwordGenerator]);
 
+  const copyPassword = async () => {
+    try {
+      await navigator.clipboard.writeText(password);
+      alert("Password copied to clipboard successfully!");
+    } catch (err) {
+      console.error("Failed to copy text: ", err);
+      alert("Failed to copy password.");
+    }
+  };
+
   return (
     <div className="flex flex-col items-center w-full h-screen bg-zinc-950">
       {/* Page Title */}
-      <h1 className="pt-14 text-4xl font-black text-center text-zinc-600">
+      <h1 className="pt-14 text-4xl font-black text-center text-emerald-500">
         Password Generator
       </h1>
-      <div className="p-8 my-8 bg-zinc-900 rounded-lg w-4/5 max-w-xl">
+      <div className="px-8 py-10 mt-12 bg-zinc-900 rounded-lg w-4/5 max-w-xl shadow-md shadow-zinc-600">
         {/* Password Input & Copy Btn */}
         <div className="flex items-center justify-center">
           <input
             type="text"
             value={password}
-            className="w-full px-4 pt-2.5 pb-3.5 font-bold text-white rounded-l-md outline-none bg-zinc-700 placeholder:text-zinc-500 cursor-not-allowed"
+            className="w-full px-4 pt-2.5 pb-3.5 font-bold text-white rounded-l-md outline-none bg-zinc-700 placeholder:text-zinc-500 cursor-not-allowed tracking-wider"
             placeholder="password"
             readOnly
           />
-          <button className="px-6 pt-2.5 pb-3.5 font-bold text-black outline-none rounded-r-md bg-emerald-600">
+          <button
+            className="px-6 pt-2.5 pb-3.5 font-bold text-black outline-none rounded-r-md bg-emerald-500 hover:bg-emerald-600"
+            onClick={copyPassword}
+          >
             Copy!
           </button>
         </div>
         {/* Options */}
-        <div className="flex items-center justify-between px-6 text-white font-medium mt-4 gap-x-6 flex-wrap gap-5">
+        <div className="flex items-center justify-between px-6 text-white font-medium mt-8 gap-x-6 flex-wrap gap-5">
           <div className="flex flex-col gap-y-2">
             <label htmlFor="range">Length: {length}</label>
             <input
@@ -61,28 +74,43 @@ function App() {
               }}
             />
           </div>
-          <div className="flex items-center gap-x-2">
+          <div className="flex items-center gap-x-2 cursor-pointer">
             <input
               type="checkbox"
               defaultChecked={numberAllowed}
               id="numberInput"
+              className="cursor-pointer"
               onChange={() => {
                 setNumberAllowed((preVal) => !preVal);
               }}
             />
-            <label htmlFor="numberInput">Number?</label>
+            <label htmlFor="numberInput" className="cursor-pointer">
+              Number?
+            </label>
           </div>
           <div className="flex items-center gap-x-2">
             <input
               type="checkbox"
               defaultChecked={characterAllowed}
               id="characterAllowed"
+              className="cursor-pointer"
               onChange={() => {
                 setCharacterAllowed((preVal) => !preVal);
               }}
             />
-            <label htmlFor="characterAllowed">Characters?</label>
+            <label htmlFor="characterAllowed" className="cursor-pointer">
+              Characters?
+            </label>
           </div>
+        </div>
+        {/* Refresh Password Button */}
+        <div className="flex items-center justify-center mt-8">
+          <button
+            className="px-6 py-3 bg-emerald-500 rounded-md font-bold hover:bg-emerald-600"
+            onClick={passwordGenerator}
+          >
+            Refresh Password
+          </button>
         </div>
       </div>
     </div>
